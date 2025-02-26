3a9d27b31744d463fe79fe4aabc6d6763b5a1248
Trim21 <trim21.me@gmail.com>
2025-02-26 21:59:12 +0800

    remove common out tests

--- lib/modules/manager/pixi/update.spec.ts
+++ lib/modules/manager/pixi/update.spec.ts
@@ -47,24 +47,5 @@ describe('modules/manager/pixi/update', () => {
       });
       expect(actual).toEqual(content);
     });
-
-    // it('updates', () => {
-    //   const { bumpedContent } = projectUpdater.bumpPackageVersion(
-    //     content,
-    //     '0.0.1',
-    //     'minor',
-    //   );
-    //   const expected = content.replace('0.0.2', '0.1.0');
-    //   expect(bumpedContent).toEqual(expected);
-    // });
-
-    // it('returns content if bumping errors', () => {
-    //   const { bumpedContent } = projectUpdater.bumpPackageVersion(
-    //     content,
-    //     '0.0.2',
-    //     true as any,
-    //   );
-    //   expect(bumpedContent).toEqual(content);
-    // });
   });
 });
